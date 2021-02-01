ba158865bffcbec675e7fb8acec73b56b7a93c57
mounirlahraichi17 <61480252+mounirlahraichi17@users.noreply.github.com>
2021-02-02 00:29:49 +0100

    Add files via upload

---
+++ cours/src/main/java/com/example/cours/CoursApplication.java
@@ -0,0 +1,26 @@
+package com.example.cours;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+@EnableMongoRepositories("com.example.repositrory")
+@ComponentScan("com.example")
+@EnableSwagger2
+public class CoursApplication  {
+
+
+}
